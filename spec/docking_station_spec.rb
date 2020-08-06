48e59b17c7f6d995dc9223314b509213d628357f
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-06 14:34:25 +0100

    added test for fail condition whe ntrying to release bike that isn't there, dockingstation only able to release bikes already there, not make new ones, added fail condition when trying to release bike not docked

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,28 +4,15 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it "releases working bikes" do
-    bike = subject.release_bike
+    bike = Bike.new
+    subject.dock(bike)
+    subject.release_bike
     expect(bike).to be_working
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
-  # it 'returns docked bikes' do
-  #   bike = Bike.new
-  #   subject.dock(bike)
-  #   expect(subject.bike).to eq bike
-  # end
-
-  # describe '#release_bike' do
-  #  it "Release a bike" do
-  #   bike = Bike.new
-  #   subject.dock(bike)
-  #   expect(subject.release_bike).to eq bike
-  #  end
-  # end
-  #  it "raises an error when there are no bikes available" do
-  #   expect {subject.release_bike}.to raise_error "No bikes available"
-  # end
-
- 
+  it 'gives an error when you try to release a bike if none are available' do
+    expect { subject.release_bike }.to raise_error 'No bikes available'
+  end
 end
